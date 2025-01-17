cd634361c15e06f7862de9bc9c587034469568e8
Tales de Oliveira <tales.oliveira032@gmail.com>
2025-01-17 12:22:21 -0300

    feat: implentação do CRUD no UserService.ts

--- config/src/domains/User/services/UserService.ts
+++ config/src/domains/User/services/UserService.ts
@@ -3,6 +3,8 @@ import prisma from "../../../../../config/prismaClient";
 
 
 class UserService{
+
+    // C - CRUD, criação do body
     async create(body: User){
        const user =  await prisma.user.create({
             data: {
@@ -17,39 +19,68 @@ class UserService{
         return user;
 
     }
-<<<<<<< HEAD
 
-    async updateUser(id: string, body: User) {
-		const user = await this.getUserbyId(id); 
-		// Busca usuário cujos dados queremos editar para verificar sua existência.
-		
-		const updatedUser = await prisma.user.update({
-			data: {
-				name:body.name,
+ 
+    // R - CRUD - Leitura dos usuários da database manipulaçao do CRUD
+    
+    async getUsers() {
+        const users = await prisma.user.findMany( { orderBy: { name: 'asc' }}); 
+        return users;
+    }
+ 
+    async getUserbyEmail(wantedEmail: string) {
+        const user = await prisma.user.findFirst({ where: { email: wantedEmail } });
+        return user;
+    }
+
+    async getUserbyId(id: number) {
+        const user = await prisma.user.findUnique({
+          where: { id },
+        });
+    
+        if (!user) {
+          throw new Error(`Id  ${id} não encontrado`);
+        }
+    
+        return user;
+      }
+
+ 
+
+  // U - CRUD - Update de algum usuário baseado no ID
+    async updateUser(id: number, body: User) {
+        const user = await this.getUserbyId(id); 
+  
+
+        const updatedUser = await prisma.user.update({
+            data: {
+                name:body.name,
                 email: body.email,
                 photo: body.photo,
                 password: body.password,
                 role: body.role
-			},
-			where: {
-				id: id
-			}
-		}); // Atualizamos esse usuário com os novos dados.
-		return updatedUser;
-=======
-    async getUsers() {
-		const users = await prisma.user.findMany( { orderBy: { name: 'asc' }}); 
-		return users;
+            },
+            where: {
+                id: id
+            }
+        }); 
+        return updatedUser;
+    }
+
+
+    async deleteUser(wantedId: number) {
+		const user = await this.getUserbyId(wantedId);
+	
+		
+		if (user) {
+			await prisma.user.delete(({ where: { id: wantedId } }));
+		} 
 	}
-    async getUserbyId(wantedId: string) {
-		const user = await prisma.user.findFirst({ where: { id: wantedId } });
-		return user; // Retorna esse usuário.
-	}
-    async getUserbyEmail(wantedEmail: string) {
-		const user = await prisma.user.findFirst({ where: { email: wantedEmail } });
-		return user;
->>>>>>> 966b5106b3328d52f9252e5eb16e7e364b76a7b7
-	}
+
+
+
+
+   
 
 }
 
