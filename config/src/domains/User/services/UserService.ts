4bc343a8d7c6bdeaf1decf05c7a933580390ca32
Tales de Oliveira <tales.oliveira032@gmail.com>
2025-01-17 12:03:13 -0300

    feat: CRUD - Update

--- config/src/domains/User/services/UserService.ts
+++ config/src/domains/User/services/UserService.ts
@@ -1,6 +1,7 @@
 import { User } from "@prisma/client";
 import prisma from "../../../../../config/prismaClient";
 
+
 class UserService{
     async create(body: User){
        const user =  await prisma.user.create({
@@ -16,6 +17,26 @@ class UserService{
         return user;
 
     }
+
+    async updateUser(id: string, body: User) {
+		const user = await this.getUserbyId(id); 
+		// Busca usuário cujos dados queremos editar para verificar sua existência.
+		
+		const updatedUser = await prisma.user.update({
+			data: {
+				name:body.name,
+                email: body.email,
+                photo: body.photo,
+                password: body.password,
+                role: body.role
+			},
+			where: {
+				id: id
+			}
+		}); // Atualizamos esse usuário com os novos dados.
+		return updatedUser;
+	}
+
 }
 
 export default new UserService();
