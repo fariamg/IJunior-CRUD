3514c21307dd06e7263de34f23323579d5e361fe
Tales de Oliveira <tales.oliveira032@gmail.com>
2025-01-17 12:38:42 -0300

    feat: testes no index adicionados

--- index.ts
+++ index.ts
@@ -1,5 +1,9 @@
 import UserService from "./config/src/domains/User/services/UserService";
 
+
+// CREATE
+
+
 async function main(){
     const body = {
         id: 0,
@@ -15,4 +19,27 @@ async function main(){
     console.log(user);
 }
 
-main()
+
+// UPDATE
+
+const id = 1;
+async function main2(){
+    const body = {
+        id: 1,
+        name:"Teste5",
+        email: "teste5@gmail.com",
+        photo: null,
+        password: "teste5",
+        role: "admin2"
+    }
+
+    const user = await UserService.updateUser(id, body)
+    console.log(user);
+    
+}
+
+
+
+
+main2()
+
