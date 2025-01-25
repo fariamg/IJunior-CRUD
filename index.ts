50187e2d691f7e7f50cb755f8b879fbf76304194
Tales de Oliveira <tales.oliveira032@gmail.com>
2025-01-25 12:56:36 -0300

    add dot.env and port initialization

--- index.ts
+++ index.ts
@@ -5,7 +5,14 @@ import CountryService from "./config/src/domains/Country/services/CountryService
 import prisma from "./config/prismaClient";
 import { Role } from "@prisma/client";
 import { Artist } from '@prisma/client';
+import { app } from "./config/expressConfig";
+import dotenv from "dotenv";
 
+dotenv.config();
+
+app.listen(process.env.PORT, () => {
+    console.log("Servidor hosteado na porta " + process.env.PORT)
+})
 //CREATE
 //(async () => {
     // console.log(await CountryService.createCountry({
