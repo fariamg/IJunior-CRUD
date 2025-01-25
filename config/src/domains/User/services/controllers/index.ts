a0a6158171c5fd33e41210fa06ff426ae860bf2f
Tales de Oliveira <tales.oliveira032@gmail.com>
2025-01-25 13:44:20 -0300

    add getByID config to Postman

--- config/src/domains/User/services/controllers/index.ts
+++ config/src/domains/User/services/controllers/index.ts
@@ -14,3 +14,18 @@ router.get("/", async (req: Request, res: Response, next: NextFunction) => {
     }
     
 });
+
+router.get("/:id", async(req: Request, res: Response, next: NextFunction) => {
+    try {
+        const user = await UserService.getUserbyId(Number(req.params.id));
+        res.json(user);
+        
+    } catch (error) {
+        next(error);
+        
+    }
+});
+
+
+
+export default router;
