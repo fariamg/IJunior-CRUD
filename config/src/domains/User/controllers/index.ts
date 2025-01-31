46b1bd46ac9500604f37db5cf296f3a5eab2dbf3
Tales de Oliveira <tales.oliveira032@gmail.com>
2025-01-31 13:11:03 -0300

    Updated to remove country

--- config/src/domains/User/controllers/index.ts
+++ config/src/domains/User/controllers/index.ts
@@ -4,6 +4,8 @@ import UserService from "../services/UserService";
 
 const router = Router();
 
+
+// ROTAS PARA LEITURA (GET)
 router.get("/", async (req: Request, res: Response, next: NextFunction) => {
     try {
         const users = await UserService.getUsers();
@@ -35,14 +37,59 @@ router.get("/:email" , async (req: Request, res: Response, next: NextFunction) =
     }
 });
 
-router.get("/country" , async (req: Request, res: Response, next: NextFunction) => {
+
+
+
+
+// ROTA PARA CRIAR UM USUÁRIO (POST)
+router.post("/create", async function createUser(req: Request, res: Response, next: NextFunction) {
     try {
-        const users = await UserService.getUserByCountry(req.params.country);
-        res.json(users);
-        
+        const { fullName, email, photo, password, role } = req.body;
+
+        if (!fullName || !email || !password || !role) {
+            return res.status(400).json({ message: "Todos os campos obrigatórios devem ser preenchidos!" });
+        }
+
+        const user = await UserService.createUser(req.body); // Passando req.body diretamente
+
+        res.status(201).json(user);
     } catch (error) {
         next(error);
-        
+    }
+});
+
+// ROTA PARA ATUALIZAR UM USUÁRIO (PUT)
+router.put("/:id", async function createUser(req: Request, res: Response, next: NextFunction) {
+    
+    try {
+        const { fullName, email, photo, password, role } = req.body;
+        const { id } = req.params; 
+
+        // Passando tanto o id quanto o body para o método updateUser
+        const user = await UserService.updateUser(Number(id), req.body);
+
+        res.status(200).json(user); // Use o status 200 para sucesso na atualização
+    } catch (error) {
+        next(error);
+    }
+});
+
+router.delete("/:id", async function createUser(req: Request, res: Response, next: NextFunction) {
+    
+    try {
+
+        const { id } = req.params; 
+
+    
+        const user = await UserService.deleteUser(Number(id));
+
+        res.status(200).json({
+            message: `Usuário com ID ${id} deletado com sucesso!`,
+            
+        }); // Use o status 200 para sucesso na atualização
+
+    } catch (error) {
+        next(error);
     }
 });
 
