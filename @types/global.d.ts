a2dc31d4b119d5691c360ccedb8a047b2e6289cc
Tales de Oliveira <tales.oliveira032@gmail.com>
2025-02-07 13:39:04 -0300

    add .env to global

---
+++ @types/global.d.ts
@@ -0,0 +1,23 @@
+import { User } from "@prisma/client";
+
+
+declare global {
+    namespace Express {
+        interface Request{
+            user:User;
+        }
+    }
+
+    namespace NodeJS{
+        interface ProcessEnv{
+            APP_URL: string;
+            PORT: number;
+            DATABASE_URL: string;
+            SECRET_KEY: string;
+            JWT_EXPIRATION: string;
+            NODE_ENV: string;
+        }
+    }
+
+
+}
