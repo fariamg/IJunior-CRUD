75968ed50d90f969336bdd7d9603c2e8db137dbb
Gabriel <fariamg4@gmail.com>
2025-02-08 19:43:48 -0300

    feat: add routes for Payment, Playlist, and Subscription controllers in express configuration

--- config/expressConfig.ts
+++ config/expressConfig.ts
@@ -2,10 +2,15 @@ import cors, { CorsOptions } from "cors";
 import dotenv from "dotenv";
 import express, { Express }  from  "express";
 import UserRouter from "../src/domains/User/controllers/UserController";
-import MusicRouter from "../src/domains/Music/controllers/index"
+import MusicRouter from "../src/domains/Music/controllers/MusicController"
 import ArtistRouter from "../src/domains/Artist/controllers/ArtistController"
 import CountryRouter from "../src/domains/Country/controllers/CountryController"
 import LikeRouter from "../src/domains/Like/controllers/LikeController"
+import PaymentRouter from "../src/domains/Payment/controllers/PaymentController"
+import PlaylistRouter from "../src/domains/Playlist/controllers/PlaylistController"
+import SubscriptionRouter from "../src/domains/Subscription/controllers/SubscriptionController"
+
+
 
 dotenv.config();
 
@@ -26,4 +31,7 @@ app.use("/api/music", MusicRouter);
 app.use("/api/artists", ArtistRouter);
 app.use("/api/countries", CountryRouter);
 app.use("/api/likes", LikeRouter);
+app.use("/api/payments", PaymentRouter);
+app.use("/api/playlists", PlaylistRouter);
+app.use("/api/subscriptions", SubscriptionRouter);
 // configurar rota de user.
