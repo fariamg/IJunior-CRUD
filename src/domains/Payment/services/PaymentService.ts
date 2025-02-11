6d816d1c489face76fc3a0bfe77282645f923ad2
Gabriel <fariamg4@gmail.com>
2025-02-11 20:25:33 -0300

    feat: enhance PaymentService with improved validation and error handling

--- src/domains/Payment/services/PaymentService.ts
+++ src/domains/Payment/services/PaymentService.ts
@@ -1,73 +1,91 @@
 import { Payment, Status } from "@prisma/client";
 import prisma from "../../../../config/prismaClient";
+import { InvalidParamError } from "../../../../errors/InvalidParamError";
+import { NotFoundError } from "../../../../errors/NotFoundError";
 
 class PaymentService {
-    async createPayment(userId: number, amount: number, status: Status, subscriptionId?: number) {
-        try {
-            return await prisma.payment.create({
-                data: {
-                    userId,
-                    amount,
-                    status,
-                    subscriptionId,
-                },
-            });
-        } catch (error) {
-            throw new Error("Erro ao criar pagamento");
+    async createPayment(userId: number, amount: number, status: Status, subscriptionId: number) {
+        if (userId == null || isNaN(userId)) {
+            throw new InvalidParamError("Insira um id de usuário válido");
         }
+
+        if (amount == null || isNaN(amount)) {
+            throw new InvalidParamError("Insira um valor válido");
+        }
+
+        if (subscriptionId == null || isNaN(subscriptionId)) {
+            throw new InvalidParamError("Insira um id de assinatura válido");
+        }
+
+        const payment = prisma.payment.create({
+            data: {
+                userId,
+                amount,
+                status,
+                subscriptionId,
+            },
+        });
     }
 
     async getPayments() {
-        try {
-            return await prisma.payment.findMany({
-                include: { user:  { omit: { password: true } }, subscription: true },
-            });
-        } catch (error) {
-            throw new Error("Erro ao buscar pagamentos");
+        const payments = await prisma.payment.findMany({
+            include: { 
+                subscription: true,
+                user:  { 
+                    omit: { 
+                        password: true }},
+            },
+        });
+
+        if (!payments.length) {
+            throw new NotFoundError("Nenhum pagamento encontrado");
         }
+
+        throw new Error("Erro ao buscar pagamentos");
     }
 
     async getPaymentsByUserId(userId: number) {
-        try {
-            return await prisma.payment.findMany({
-                where: { userId },
-                include: { subscription: true },
-            });
-        } catch (error) {
-            throw new Error("Erro ao buscar pagamentos");
+        if (userId == null || isNaN(userId)) {
+            throw new InvalidParamError("Insira um id de usuário válido");
+        }
+
+        const payments = await prisma.payment.findMany({
+            where: { userId },
+            include: { subscription: true },
+        });
+
+        if (!payments.length) {
+            throw new NotFoundError("Nenhum pagamento encontrado");
         }
     }
 
     async getPaymentById(paymentId: number) {
-        try {
-            return await prisma.payment.findUnique({
-                where: { id: paymentId },
-                include: { user:  { omit: { password: true } }, subscription: true },
-            });
-        } catch (error) {
-            throw new Error("Erro ao buscar pagamento");
+        if (paymentId == null || isNaN(paymentId)) {
+            throw new InvalidParamError("Insira um id de pagamento válido");
+        }
+        
+        const payment = await prisma.payment.findUnique({
+            where: { id: paymentId },
+            include: { 
+                subscription: true, 
+                user:  { 
+                    omit: { 
+                        password: true }},
+            },
+        });
+
+        if (!payment) {
+            throw new NotFoundError(`Pagamento com id ${paymentId} não encontrado`);
         }
     }
 
     async updatePaymentStatus(paymentId: number, status: Status) {
-        try {
-            return await prisma.payment.update({
-                where: { id: paymentId },
-                data: { status },
-            });
-        } catch (error) {
-            throw new Error("Erro ao atualizar status do pagamento");
-        }
-    }
+        await this.getPaymentById(paymentId);
 
-    private async deletePayment(paymentId: number) {
-        try {
-            return await prisma.payment.delete({
-                where: { id: paymentId },
-            });
-        } catch (error) {
-            throw new Error("Erro ao excluir pagamento");
-        }
+        return await prisma.payment.update({
+            where: { id: paymentId },
+            data: { status },
+        });
     }
 }
 
