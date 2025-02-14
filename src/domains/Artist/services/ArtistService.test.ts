7feeca677402e34eee02de31636307e9b615bc0b
Gabriel <fariamg4@gmail.com>
2025-02-14 20:25:26 -0300

    test: add error handling tests for missing artist name and country ID in ArtistService

--- src/domains/Artist/services/ArtistService.test.ts
+++ src/domains/Artist/services/ArtistService.test.ts
@@ -1,6 +1,8 @@
 import { describe, test, expect } from '@jest/globals';
 import { prismaMock } from '../../../../config/singleton';
 import artistService from './ArtistService';
+import { InvalidParamError } from '../../../../errors/InvalidParamError';
+import prisma from '../../../../config/client';
 
 describe('create new artist', () => {
     test('Deve criar um novo artista', async () => { 
@@ -35,17 +37,29 @@ describe('create new artist', () => {
         expect(savedArtist.name).toEqual(artist.name);
     });
     
-    // test('Deve lançar um erro se o nome do artista não for informado', async () => {
-    //     const artist = {
-    //         name:undefined as unknown as string,
-    //         photo: 'photo',
-    //         bio: 'bio',
-    //         listeners: 0,
-    //         countryId: 1,
-    //     };
+    test('Deve lançar um erro se o nome do artista não for informado', async () => {
+        const artist = {
+            name: undefined as unknown as string,
+            photo: 'photo',
+            bio: 'bio',
+            listeners: 0,
+            countryId: 1,
+        };
 
-    //     await expect(artistService.createArtist(artist)).rejects.toThrowError('Nome do artista não informado!');
-    // });
+        await expect(artistService.createArtist(artist)).rejects.toThrow(InvalidParamError);
+    });
+
+    test('Deve lançar um erro se o id do país não for informado', async () => {
+        const artist = {
+            name: 'artist',
+            photo: 'photo',
+            bio: 'bio',
+            listeners: 0,
+            countryId: undefined as unknown as number,
+        };
+
+        await expect(artistService.createArtist(artist)).rejects.toThrow(InvalidParamError);
+    });
 });
 
 
